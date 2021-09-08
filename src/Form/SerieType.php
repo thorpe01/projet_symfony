c9f914a4c56d48fa81ecd8583b603b79f11f885b
thorpe01 <exercice0007@gmail.com>
2021-09-08 16:49:27 +0200

    css

--- src/Form/SerieType.php
+++ src/Form/SerieType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 class SerieType extends AbstractType
 {
